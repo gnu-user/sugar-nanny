ad38d3da8982c73d60983ec8f3e3daca4850533f
gnu_user <gsoc.student@gmail.com>
2016-01-23 14:45:22 -0500

    Updated deploy scripts

--- setup/deploy/nginx.sh
+++ setup/deploy/nginx.sh
@@ -4,10 +4,5 @@
 apt-get install -y nginx nginx-extras
 cp -vf ../config/nginx.conf /etc/nginx/
 cp -vf ../config/default /etc/nginx/sites-enabled/
-# Setup SSL certs
-cp -vrf ../config/.certs ~/
-chmod 0600 ~/.certs/*.pem
-chmod 0644 ~/.certs/*.crt
-chmod 0644 ~/.certs/*.csr
 # Restart nginx
 service nginx restart
