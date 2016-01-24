d09ec9bdeb47ad6c7a9fc691285d86cbffe89785
gnu_user <gsoc.student@gmail.com>
2016-01-24 04:12:57 -0500

    Added frontent www template

---
+++ www/assets/app.js
@@ -0,0 +1,126 @@
+(function(){
+  var app = angular.module('nanny',['ui.router', 'ngRoute']);
+
+  app.config(function ($routeProvider, $locationProvider, $stateProvider, $urlRouterProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: "views/dashboard.html",
+        controller: "DashCtrl"
+      })
+      .when('/about', {
+        templateUrl: "views/about.html",
+        controller: "DashCtrl"
+      })
+      .when('/readings', {
+        templateUrl: "views/readings.html",
+        controller: "DashCtrl"
+      })
+      .when('/meals', {
+        templateUrl: "views/meals.html",
+        controller: "DashCtrl"
+      })
+      .when('/history', {
+        templateUrl: "views/history.html",
+        controller: "DashCtrl"
+      })
+      .when('/foodInformation', {
+        templateUrl: "views/foodInfo.html",
+        controller: "DashCtrl"
+      })
+
+
+      ;
+
+    $locationProvider.html5Mode(false);
+  });
+
+  app.controller('GlobalCtrl', function($rootScope, $scope, $location, $routeParams, $http){
+    $scope.notifications = [
+      {
+        "txt":"test",
+        "time":"notification time"
+      }
+    ]
+    $rootScope.$on( "$routeChangeStart", function(event, next, current) {
+      var page = $location.path().substr(1)
+      if(page != ""){
+        $scope.title = page.charAt(0).toUpperCase() + page.slice(1)
+      } else {
+        $scope.title = "Dashboard"
+      }
+
+    });
+  });
+
+  app.controller('DashCtrl', function($rootScope, $scope, $location, $routeParams, $http){
+
+    $scope.page = "Dashboard"
+
+    $http(
+      {method: 'GET',
+       url: 'http://api.sugarnanny.tech/history/blood_sugar/1'}
+     )
+          .success(function(data, status, headers, config) {
+              $scope.sugarData = data.data;
+
+          }).
+            error(function(data, status, headers, config) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+            console.log('page not found:', data);
+          });
+
+    $http({method: 'GET', url: 'http://api.sugarnanny.tech/stats/insulin/1'})
+
+          .success(function(data, status, headers, config) {
+              $scope.insulinIntake = data.data
+          }).
+            error(function(data, status, headers, config) {
+            console.log('page not found:', data);
+          });
+
+    $http({method: 'GET', url: 'http://api.sugarnanny.tech/stats/meals/1'})
+          .success(function(data, status, headers, config) {
+              $scope.mealsHistory = data.data
+
+          }).
+            error(function(data, status, headers, config) {
+            console.log('page not found:', data);
+          });
+
+
+    $scope.doSearch = function() {
+      if($scope.search.length > 3){
+
+          $http({method: 'GET', url: 'http://jsonplaceholder.typicode.com/users'}).
+            success(function(data, status, headers, config) {
+              $scope.searchResults = data
+          }).
+            error(function(data, status, headers, config) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+            console.log('page not found:', data);
+          });
+      };
+    };
+
+    $scope.foodSelected = function(rows) {
+
+      $http({
+        method: 'GET',
+        url: 'http://jsonplaceholder.typicode.com/users'
+      }).
+        success(function(data, status, headers, config) {
+          $scope.foodInformation = data;
+          $location.url('/meal');
+      }).
+        error(function(data, status, headers, config) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+        console.log('page not found:', data);
+      });
+    };
+
+  });
+// root controller for home page
+})();
