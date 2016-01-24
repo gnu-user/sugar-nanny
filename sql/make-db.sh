fbb543a16ce72466fbfdf37b45ce59b67aace426
gnu_user <gsoc.student@gmail.com>
2016-01-23 23:37:26 -0500

    Fixed the script again

--- sql/make-db.sh
+++ sql/make-db.sh
@@ -4,15 +4,15 @@ HOST=$3
 
 if [[ -n "${USER}" && -n "${PASS}" && -n "${HOST}" ]]
 then
-    PGPASSWORD=${PASS} dropdb -U${USER} --password ${PASS} -h ${HOST} sugarnanny
-    PGPASSWORD=${PASS} createdb -U${USER} --password ${PASS} -h ${HOST} sugarnanny
-    PGPASSWORD=${PASS} pg_restore -U${USER} --password ${PASS} -h ${HOST} -d sugarnanny openfoodfacts.dump
-    PGPASSWORD=${PASS} psql -U${USER} --password ${PASS} -h ${HOST} sugarnanny < 0001-schema.sql
-    PGPASSWORD=${PASS} psql -U${USER} --password ${PASS} -h ${HOST} sugarnanny < 0002-blood_sugar_functions.sql
-    PGPASSWORD=${PASS} psql -U${USER} --password ${PASS} -h ${HOST} sugarnanny < 0003-account-functions.sql
-    PGPASSWORD=${PASS} psql -U${USER} --password ${PASS} -h ${HOST} sugarnanny < 0004-food_functions.sql
-    PGPASSWORD=${PASS} psql -U${USER} --password ${PASS} -h ${HOST} sugarnanny < 0005-stats-functions.sql
-    PGPASSWORD=${PASS} psql -U${USER} --password ${PASS} -h ${HOST} sugarnanny < 9999-data.sql
+    PGPASSWORD=${PASS} dropdb -U${USER} -h ${HOST} sugarnanny
+    PGPASSWORD=${PASS} createdb -U${USER} -h ${HOST} sugarnanny
+    PGPASSWORD=${PASS} pg_restore -U${USER} -h ${HOST} -d sugarnanny openfoodfacts.dump
+    PGPASSWORD=${PASS} psql -U${USER} -h ${HOST} sugarnanny < 0001-schema.sql
+    PGPASSWORD=${PASS} psql -U${USER} -h ${HOST} sugarnanny < 0002-blood_sugar_functions.sql
+    PGPASSWORD=${PASS} psql -U${USER} -h ${HOST} sugarnanny < 0003-account-functions.sql
+    PGPASSWORD=${PASS} psql -U${USER} -h ${HOST} sugarnanny < 0004-food_functions.sql
+    PGPASSWORD=${PASS} psql -U${USER} -h ${HOST} sugarnanny < 0005-stats-functions.sql
+    PGPASSWORD=${PASS} psql -U${USER} -h ${HOST} sugarnanny < 9999-data.sql
     python2 make-readings.py ${USER} ${PASS} ${HOST}
 else
     dropdb -U${USER} --password ${PASS} -h ${HOST} sugarnanny
