742fac5e298d2d876ce0f79e2b569eb7291bb12d
Eric Matthews <matthew3@uwindsor.ca>
2016-01-23 23:21:34 -0500

    Two food routes (/food/retrieve/, /food/record)

--- sql/make-db.sh
+++ sql/make-db.sh
@@ -4,5 +4,6 @@ pg_restore -d sugarnanny openfoodfacts.dump
 psql sugarnanny < 0001-schema.sql
 psql sugarnanny < 0002-blood_sugar_functions.sql
 psql sugarnanny < 0003-account-functions.sql
+psql sugarnanny < 0004-food_functions.sql
 psql sugarnanny < 9999-data.sql
 python2 make-readings.py
