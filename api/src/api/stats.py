b56cc74b7ee7884324ef7acba99df1cf534979a9
gnu_user <gsoc.student@gmail.com>
2016-01-23 23:05:29 -0500

    Implemented stats routes

--- api/src/api/stats.py
+++ api/src/api/stats.py
@@ -7,55 +7,43 @@ from utils import (success_response,
 stats = Blueprint('stats', __name__)
 
 
-@stats.route('/blood_sugar/<user_uuid>', methods=['GET'])
+@stats.route('/blood_sugar/<account_id>', methods=['GET'])
 @validate_response()
-def stats_blood_sugar(user_uuid):
+def stats_blood_sugar(account_id):
     with get_db_cursor(commit=True) as cur:
         cur.execute('''
-                    SELECT json_agg(summarize_listing(listings))::jsonb AS response
-                    FROM listings
-                    JOIN favorite_listings
-                    USING (listing_id)
-                    WHERE favorite_listings.account_id =
-                      (SELECT account_id
-                       FROM accounts
-                       WHERE account_uuid = %s)
-                    ''', (user_uuid,))
+                    SELECT json_agg(summarize_readings(readings))::jsonb AS response
+                    FROM readings
+                    WHERE account_id = %s
+                    AND reading_timestamp >= (now() - interval '48 hour');
+                    ''', (account_id,))
         res = cur.fetchone()['response']
-    return success_response({'data': {'results': res}})
+    return success_response({'data': res})
 
 
-@stats.route('/insulin/<user_uuid>', methods=['GET'])
+@stats.route('/insulin/<account_id>', methods=['GET'])
 @validate_response()
-def stats_insulin(user_uuid):
+def stats_insulin(account_id):
     with get_db_cursor(commit=True) as cur:
         cur.execute('''
-                    SELECT json_agg(summarize_listing(listings))::jsonb AS response
-                    FROM listings
-                    JOIN favorite_listings
-                    USING (listing_id)
-                    WHERE favorite_listings.account_id =
-                      (SELECT account_id
-                       FROM accounts
-                       WHERE account_uuid = %s)
-                    ''', (user_uuid,))
+                    SELECT json_agg(summarize_doses(doses))::jsonb AS response
+                    FROM doses
+                    WHERE account_id = %s
+                    AND dose_timestamp >= (now() - interval '48 hour');
+                    ''', (account_id,))
         res = cur.fetchone()['response']
-    return success_response({'data': {'results': res}})
+    return success_response({'data': res})
 
 
-@stats.route('/meals/<user_uuid>', methods=['GET'])
+@stats.route('/meals/<account_id>', methods=['GET'])
 @validate_response()
-def stats_meals(user_uuid):
+def stats_meals(account_id):
     with get_db_cursor(commit=True) as cur:
         cur.execute('''
-                    SELECT json_agg(summarize_listing(listings))::jsonb AS response
-                    FROM listings
-                    JOIN favorite_listings
-                    USING (listing_id)
-                    WHERE favorite_listings.account_id =
-                      (SELECT account_id
-                       FROM accounts
-                       WHERE account_uuid = %s)
-                    ''', (user_uuid,))
+                    SELECT json_agg(summarize_food_history(food_history))::jsonb AS response
+                    FROM food_history
+                    WHERE account_id = %s
+                    AND food_timestamp >= (now() - interval '48 hour');
+                    ''', (account_id,))
         res = cur.fetchone()['response']
-    return success_response({'data': {'results': res}})
+    return success_response({'data': res})
