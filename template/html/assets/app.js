a3c3ae4436cdc388794c68ef6eca1217cf807241
Dennis Pacewicz <dennis.pacewicz@gmail.com>
2016-01-24 02:50:37 -0500

    cleaned up history view

--- template/html/assets/app.js
+++ template/html/assets/app.js
@@ -58,7 +58,7 @@
 
     $http(
       {method: 'GET',
-       url: 'http://api.sugarnanny.tech/history/blood_sugar/1'}
+       url: 'http://api.sugarnanny.tech/stats/blood_sugar/1'}
      )
           .success(function(data, status, headers, config) {
               $scope.sugarData = data.data;
@@ -69,11 +69,12 @@
             // or server returns response with an error status.
             console.log('page not found:', data);
           });
-
+//http://api.sugarnanny.tech/history/insulin/1
     $http({method: 'GET', url: 'http://api.sugarnanny.tech/stats/insulin/1'})
 
           .success(function(data, status, headers, config) {
               $scope.insulinIntake = data.data
+              console.log(data.data)
           }).
             error(function(data, status, headers, config) {
             console.log('page not found:', data);
@@ -82,7 +83,7 @@
     $http({method: 'GET', url: 'http://api.sugarnanny.tech/stats/meals/1'})
           .success(function(data, status, headers, config) {
               $scope.mealsHistory = data.data
-
+              console.log(data.data)
           }).
             error(function(data, status, headers, config) {
             console.log('page not found:', data);
